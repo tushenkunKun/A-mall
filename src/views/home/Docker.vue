7baf2ea08671b1948d5c5e99f51e9e45c39f4d2d
anlikun <an18622575579@163.com>
2022-03-29 13:37:44 +0800

    开发组件Docker

--- src/views/home/Docker.vue
+++ src/views/home/Docker.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="docker">
-    <div class="docker__item">
+    <div class="docker__item docker__item_active">
       <span class="doker__item__icon">&#xe608;</span>
       <span class="docker__item__text">首页</span>
     </div>
@@ -28,27 +28,35 @@ export default {
 </script>
 <style lang="scss" scoped>
 .docker {
-  width: 375px;
-  height: 49px;
-  position: absolute;
+  width: 375rem;
+  height: 49rem;
+  position: fixed;
   bottom: 0;
   left: 0;
   right: 0;
+  background: #ffffff;
+	box-shadow: 0 -1rem 1rem 0 #f1f1f1;
   display: flex;
   justify-content: space-evenly;
   align-items: center;
+  padding-top: 10rem;
   &__item {
     display: flex;
     flex-direction: column;
     align-items: center;
-    &__icon {
-      // font-size: 20px;
+    &__icon{
+      font-size: 20rem;
     }
     &__text {
       font-family: PingFangSC-Regular;
-      font-size: 10px;
+      font-size: 25rem;
       text-align: center;
-      margin-top: 2px;
+      margin-top: 2rem;
+      transform: scale(.5,.5);
+      transform-origin: center top;
+    }
+    &_active{
+      color: #1fa4fc;
     }
   }
 }
