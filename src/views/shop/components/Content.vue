3dcc5682fc3a22aaf81b11086cdf87fe89b25c2f
anlikun <an18622575579@163.com>
2022-04-11 18:35:49 +0800

    具体店铺页面中的左边导航和右边内容

--- src/views/shop/components/Content.vue
+++ src/views/shop/components/Content.vue
@@ -1,21 +1,194 @@
 <template>
-  <div class="shop-content">123</div>
+  <div class="shop-content">
+    <div class="shop-content__nav">
+      <div
+        :class="{ 'shop-content__nav__item': true, 'shop-content__nav__item--active': item.name === currentName }"
+        v-for="(item, index) of shopNav"
+        :key="index"
+        :tab-name="item.name"
+        @click="checkedNav(item.name)"
+      >
+        {{ item.text }}
+      </div>
+    </div>
+    <div class="shop-content__list">
+      <div class="shop-content__list__item" v-for="item of shopList" :key="item.id">
+        <div class="shop-content__list__item__img">
+          <img :src="item.imgUrl" alt="" />
+        </div>
+        <div class="shop-content__list__item__desc">
+          <div class="shop-content__list__item__desc__weight">{{item.weight}}</div>
+          <div class="shop-content__list__item__desc__sale-num">月售{{item.sales}}件</div>
+          <div class="shop-content__list__item__desc__price">
+            <div class="shop-content__list__item__desc__price__promotion-price">¥{{item.promotionPrice}}</div>
+            <div class="shop-content__list__item__desc__price__original-price">¥{{item.originalPrice}}</div>
+            <div class="shop-content__list__item__desc__price__count">
+              <button class="shop-content__list__item__desc__price__count__minus">&#xe780;</button>
+              <span class="shop-content__list__item__desc__price__count__number">88</span>
+              <button class="shop-content__list__item__desc__price__count__plus">&#xe7e0;</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
+import { ref } from "vue";
 export default {
-  name:'ShopContent',
-  setup(){
-    return{}
-  }
-}
+  name: "ShopContent",
+  setup() {
+    const shopNav = [
+      { name: "all", text: "全部商品" },
+      { name: "seckill", text: "秒杀" },
+      { name: "fruit", text: "新鲜水果" },
+      { name: "snacks", text: "休闲食品" },
+      { name: "vegetable", text: "时令蔬菜" },
+      { name: "meat", text: "肉蛋家禽" },
+    ];
+    const shopList = [
+      {
+        id: 1,
+        weight: "全部-番茄150g/份",
+        imgUrl: "https://markdown-1253389072.cos.ap-nanjing.myqcloud.com/202202261527635.png",
+        sales: 1,
+        promotionPrice: 1.6,
+        originalPrice: 1.6,
+      },
+      {
+        id: 2,
+        weight: "全部-番茄250g/份",
+        imgUrl: "https://markdown-1253389072.cos.ap-nanjing.myqcloud.com/202202261527853.png",
+        sales: 2,
+        promotionPrice: 2.6,
+        originalPrice: 2.6,
+      },
+      {
+        id: 3,
+        weight: "全部-番茄350g/份",
+        imgUrl: "https://markdown-1253389072.cos.ap-nanjing.myqcloud.com/202202261527922.png",
+        sales: 3,
+        promotionPrice: 3.6,
+        originalPrice: 3.6,
+      },
+      {
+        id: 4,
+        weight: "全部-番茄450g/份",
+        imgUrl: "https://markdown-1253389072.cos.ap-nanjing.myqcloud.com/202202261530259.png",
+        sales: 4,
+        promotionPrice: 4.6,
+        originalPrice: 4.6,
+      },
+    ];
+    const currentName = ref("all");
+    const checkedNav = (itemName) => {
+      currentName.value = itemName;
+    };
+    return { shopNav, currentName, shopList, checkedNav };
+  },
+};
 </script>
-<style lang='scss' scoped>
-.shop-content{
+<style lang="scss" scoped>
+.shop-content {
+  display: flex;
   position: absolute;
   top: 146rem;
   bottom: 49rem;
   left: 0;
   right: 0;
-  background-color: orange;
+  &__nav {
+    overflow-y: auto;
+    background-color: #f5f5f5;
+    text-align: center;
+    &__item {
+      width: 76rem;
+      height: 40rem;
+      font-family: PingFangSC-Regular;
+      font-size: 14rem;
+      color: #333;
+      line-height: 40rem;
+      &--active {
+        background-color: #fff;
+      }
+    }
+  }
+  &__list {
+    overflow-y: auto;
+    flex-grow: 1;
+    background-color: #fff;
+    &__item {
+      display: flex;
+      margin: 0 18rem 12rem 16rem;
+      border-bottom: 1rem solid #f1f1f1;
+      &__img {
+        margin-right: 16rem;
+        margin-bottom: 12rem;
+        & img {
+          width: 68rem;
+          height: 68rem;
+        }
+      }
+      &__desc {
+        flex-grow: 1;
+        &__weight {
+          font-family: PingFangSC-Medium;
+          font-size: 14rem;
+          color: #333333;
+          margin-bottom: 8rem;
+        }
+        &__sale-num {
+          font-family: PingFangSC-Regular;
+          font-size: 12rem;
+          color: #333333;
+          line-height: 16rem;
+          margin-bottom: 8rem;
+        }
+        &__price {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          &__promotion-price {
+            font-family: PingFangSC-Semibold;
+            font-size: 14rem;
+            color: #e93b3b;
+            line-height: 20rem;
+            &::first-letter {
+              font-size: 12rem;
+            }
+          }
+          &__original-price {
+            font-family: PingFangSC-Regular;
+            font-size: 10rem;
+            color: #999;
+            line-height: 20rem;
+            &::first-letter {
+              font-size: 12rem;
+            }
+          }
+          &__count {
+            display: flex;
+            align-items: center;
+            &__minus {
+              color: #666666;
+              font-size: 20rem;
+              background: none;
+            }
+            &__number {
+              font-family: PingFangSC-Regular;
+              font-size: 14rem;
+              color: #333333;
+              margin-left: 6rem;
+              margin-right: 6rem;
+            }
+            &__plus {
+              font-size: 17rem;
+              color: #0091ff;
+              background: none;
+            }
+          }
+        }
+      }
+    }
+  }
 }
 </style>
