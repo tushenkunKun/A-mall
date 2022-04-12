13aa42f53a82e3e078af355c78994ea3862b9ffc
anlikun <an18622575579@163.com>
2022-04-12 18:45:04 +0800

    Cart制作

---
+++ src/views/shop/components/Cart.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="cart">123</div>
+</template>
+<script>
+export default {
+  name: "Cart",
+  setup() {
+    return {};
+  },
+};
+</script>
+<style lang="scss" scoped>
+.cart {
+}
+</style>
