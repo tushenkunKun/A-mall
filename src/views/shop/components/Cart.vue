9ca4150384ceb60045ceb07b8e4553bb0dcef77b
anlikun <an18622575579@163.com>
2022-04-15 11:09:36 +0800

    购物车总数写为计算属性，动态改变

--- src/views/shop/components/Cart.vue
+++ src/views/shop/components/Cart.vue
@@ -3,20 +3,43 @@
     <div class="cart__info">
       <div class="cart__info__icon">
         <img src="https://markdown-1253389072.cos.ap-nanjing.myqcloud.com/202202261637089.png" alt="" />
-        <span class="cart__info__icon__number"></span>
+        <span class="cart__info__icon__number">{{ total }}</span>
       </div>
       <div class="cart__info__count">
-        总计：<span class="cart__info__count__total-price">¥128</span>
+        总计：
+        <span class="cart__info__count__total-price">¥128</span>
       </div>
     </div>
     <div class="cart__payment">去结算</div>
   </div>
 </template>
 <script>
+import { computed } from "@vue/runtime-core";
+import { useRoute } from "vue-router";
+import { useStore } from "vuex";
 export default {
   name: "Cart",
   setup() {
-    return {};
+    const route = useRoute();
+    const store = useStore();
+    // 获取店铺的id
+    const shopId = route.params.id;
+    // 获取购物车的数据
+    const cartData = store.state.cartData;
+    // 定义计算属性total
+    const total = computed(() => {
+      // 获取此店铺id下的所有商品
+      const itemList = cartData[shopId];
+      // 计算总量
+      let count = 0;
+      if (itemList) {
+        for (const key in itemList) {
+          count += itemList[key].count;
+        }
+      }
+      return count;
+    });
+    return { total };
   },
 };
 </script>
@@ -34,50 +57,50 @@ export default {
   &__info {
     display: flex;
     align-items: center;
-    &__icon{
+    &__icon {
       position: relative;
       margin: 0 32rem 0 24rem;
-      & img{
+      & img {
         width: 28rem;
         height: 26rem;
       }
-      &__number{
+      &__number {
         display: block;
-        width: 20rem;
+        min-width: 20rem;
         height: 20rem;
         background-color: #e93b3b;
-        border-radius: 100%;
-				font-family: PingFangSC-Medium;
-				font-size: 16rem;
-				transform: scale(0.5, 0.5);
-				transform-origin: center top;
-				color: #ffffff;
-				text-align: center;
-				position: absolute;
-				right: -10rem;
-				top: -4rem;
+        border-radius: 10rem;
+        font-family: PingFangSC-Medium;
+        font-size: 16rem;
+        transform: scale(0.5, 0.5);
+        transform-origin: left top;
+        color: #ffffff;
+        text-align: center;
+        position: absolute;
+        left: 24rem;
+        top: -4rem;
       }
     }
-    &__count{
+    &__count {
       font-family: PingFangSC-Regular;
-			font-size: 12rem;
-			color: #333333;
-      &__total-price{
+      font-size: 12rem;
+      color: #333333;
+      &__total-price {
         font-family: PingFangSC-Medium;
-				font-size: 18rem;
-				color: #e93b3b;
+        font-size: 18rem;
+        color: #e93b3b;
       }
     }
   }
-  &__payment{
+  &__payment {
     width: 98rem;
-		height: 49rem;
-		background-color: #4fb0f9;
-		font-family: PingFangSC-Medium;
-		font-size: 14rem;
-		color: #ffffff;
-		text-align: center;
-		line-height: 49rem;
+    height: 49rem;
+    background-color: #4fb0f9;
+    font-family: PingFangSC-Medium;
+    font-size: 14rem;
+    color: #ffffff;
+    text-align: center;
+    line-height: 49rem;
   }
 }
 </style>
