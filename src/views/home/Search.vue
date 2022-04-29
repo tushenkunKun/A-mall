a47405ff466f325ccebc9ad667909b161e836e42
anlikun <an18622575579@163.com>
2022-04-29 09:45:28 +0800

    element优化Home中的Search组件

--- src/views/home/Search.vue
+++ src/views/home/Search.vue
@@ -1,56 +1,44 @@
 <template>
   <div class="search">
     <span class="search__icon">&#xe622;</span>
-    <input type="text" class="search__input" placeholder="山姆会员商店优惠商品" />
+    <!-- <input type="text" class="search__input" placeholder="山姆会员商店优惠商品" /> -->
+    <el-input class="search__input" placeholder="山姆会员商店优惠商品" type="text" v-model="searchInputContent"></el-input>
   </div>
 </template>
 <script>
+import { ref } from '@vue/reactivity';
 export default {
   name: "Search",
   setup() {
-    return {};
+    const searchInputContent = ref("");
+    return {searchInputContent};
   },
 };
 </script>
 <style lang="scss" scoped>
 .search {
-  text-align: center;
   position: relative;
-  color: #b7b7b7;
   &__icon {
     position: absolute;
     left: 36rem;
     top: 7rem;
     font-size: 16rem;
+    z-index: 10;
+    color: gray;
   }
   &__input {
-    background: #f5f5f5;
-    border-radius: 16rem;
-    border: none;
-    width: 339rem;
-    height: 32rem;
-    padding-left: 44rem;
-    outline: none;
-    &::-webkit-input-placeholder {
-      font-family: PingFangSC-Regular;
-      font-size: 14rem;
-      line-height: 16rem;
-    }
-    &:-moz-placeholder {
-      font-family: PingFangSC-Regular;
-      font-size: 14rem;
-      line-height: 16rem;
-    }
-    &::-moz-placeholder {
-      font-family: PingFangSC-Regular;
-      font-size: 14rem;
-      line-height: 16rem;
-    }
-    &:-ms-input-placeholder {
-      font-family: PingFangSC-Regular;
-      font-size: 14rem;
-      line-height: 16rem;
-    }
+    justify-content: center;
   }
+}
+::v-deep .el-input__wrapper {
+  background: #f5f5f5;
+  border-radius: 16rem;
+  border: none;
+  width: 339rem;
+  height: 32rem;
+  padding-left: 44rem;
+  outline: none;
+  flex-grow: 0;
+  outline: none;
 }
 </style>
