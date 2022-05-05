74c2b74cd2c25190d923080150e6be2ecd0b412f
anlikun <an18622575579@163.com>
2022-05-05 11:13:58 +0800

    引入swiper插件

--- src/views/home/Banner.vue
+++ src/views/home/Banner.vue
@@ -1,21 +1,38 @@
 <template>
   <div class="banner">
-    <img src="https://markdown-1253389072.cos.ap-nanjing.myqcloud.com/202202191609589.png" alt="双11狂欢节">
+    <swiper :modules="modules" :slides-per-view="1" :space-between="10" navigation :pagination="{ clickable: true }">
+      <swiper-slide><img src="https://markdown-1253389072.cos.ap-nanjing.myqcloud.com/202202191609589.png" alt="双11狂欢节" /></swiper-slide>
+      <swiper-slide><img src="https://markdown-1253389072.cos.ap-nanjing.myqcloud.com/202202191609589.png" alt="双11狂欢节" /></swiper-slide>
+      <swiper-slide><img src="https://markdown-1253389072.cos.ap-nanjing.myqcloud.com/202202191609589.png" alt="双11狂欢节" /></swiper-slide>
+      <swiper-slide><img src="https://markdown-1253389072.cos.ap-nanjing.myqcloud.com/202202191609589.png" alt="双11狂欢节" /></swiper-slide>
+      <swiper-slide><img src="https://markdown-1253389072.cos.ap-nanjing.myqcloud.com/202202191609589.png" alt="双11狂欢节" /></swiper-slide>
+      <swiper-slide><img src="https://markdown-1253389072.cos.ap-nanjing.myqcloud.com/202202191609589.png" alt="双11狂欢节" /></swiper-slide>
+    </swiper>
+    <!-- <img src="https://markdown-1253389072.cos.ap-nanjing.myqcloud.com/202202191609589.png" alt="双11狂欢节" /> -->
   </div>
 </template>
 <script>
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 export default {
-  name:'Banner',
-  setup(){
-    return{}
-  }
-}
+  name: "Banner",
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return { modules: [Navigation, Pagination, A11y] };
+  },
+};
 </script>
-<style lang='scss' scoped>
-.banner{
+<style lang="scss" scoped>
+.banner {
   margin-top: 12rem;
   text-align: center;
-  & img{
+  & img {
     width: 339rem;
     height: 86rem;
   }
